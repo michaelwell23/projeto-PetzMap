b010b9dc1167cc469e649e2f6120bac197584346
Michael Wellington Lopes <michael23.wellington@gmail.com>
2024-05-31 13:43:39 -0300

    Configurações iniciais do backend (estrutura de projeto e banco de dados)

---
+++ server/src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+import routes from './routes';
+
+import './database/connections';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+export default app;
